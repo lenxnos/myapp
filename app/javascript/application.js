21e446368d9ed26dd77dabff75d4a7eecfc39c90
Nelson Soto <nelson.soto.c05@gmail.com>
2022-05-15 13:36:44 -0400

    chore: Configure bootstrap with rails 7

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,24 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import "popper"
+import "bootstrap"
+
+let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
+// var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+// var toastList = toastElList.map(function (toastEl) {
+//   return new bootstrap.Toast(toastEl)
+// })
+
+// const toastTrigger = document.getElementById('liveToastBtn')
+// if (toastTrigger) {
+//   toastTrigger.addEventListener('click', function () {
+//     const toast = new bootstrap.Toast(document.getElementById('liveToast'))
+//     toast.show()
+//   });
+// }
