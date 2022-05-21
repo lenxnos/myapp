baffb1811cb654cd724a5e62f9555ae54f9a1c97
Nelson Soto <nelson.soto.c05@gmail.com>
2022-05-21 19:21:58 -0400

    feat: add users show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update, :show]
+
+
+  def show
+    @articles = @user.articles
+  end
+
   def new
     @user = User.new
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to articles_path
@@ -19,7 +24,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to the site, #{@user.username}! You have successfully signed up."
       redirect_to articles_path
@@ -34,4 +38,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:username, :email, :password)
   end
 
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    email_adress = user.email.downcase
+    hash = Digest::MD5::hexdigest(email_adress)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_url, alt: user.username, class: "img-thumbnail shadow mx-auto d-block", style: "height: #{size}px; width: #{size}px; background: #e1e1e1;")
+  end
 end
