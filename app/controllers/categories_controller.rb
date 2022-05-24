e1346e40fd43b3c1f650ff6f46d6db15c795534d
Nelson Soto <nelson.soto.c05@gmail.com>
2022-05-24 00:38:17 -0400

    feat: add function test to category controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,35 @@
+class CategoriesController < ApplicationController
+  # before_action :set_category, only: [:show, :edit, :update, :destroy]
+
+  def index
+    # @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def show
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_category
+    # @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+end
